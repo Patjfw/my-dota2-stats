bd45c3f31655a6a9037e09a61270881729aa81a3
Patjfw <jfwdev@gmail.com>
2017-10-16 23:10:59 +0800

    need to get avatar

--- server/apiRequest.js
+++ server/apiRequest.js
@@ -8,6 +8,7 @@ exports.request = function (expressApp) {
   const LANG = 'zh_cn'
   const STEAM_MATCH_HISTORY_API_URL = 'https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001'
   const STEAM_MATCH_DETAIL_API_URL = 'https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001'
+  const STEAM_PLAYER_SUMMARIES_API_URL = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002'
 
   let images;
   let imagesPromise = imageCache.checkCache()
@@ -30,6 +31,11 @@ exports.request = function (expressApp) {
     }
   }
 
+  // Convert Steam User ID(32) to Steam 64 ID
+  function steamIDConvertion(steamUserID){
+
+  }
+
   expressApp.get('/get_image_cache', (req,res) => {
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(images, null, 2))
@@ -42,6 +48,11 @@ exports.request = function (expressApp) {
 
   expressApp.get('/get_match_detail', (req, res) => {
     let requestObj = assembleQueryObj('GET', STEAM_MATCH_DETAIL_API_URL, req.query)
+    request(requestObj).pipe(res);
+  })
+
+  expressApp.get('/get_player_avatar', (req, res) => {
+    console.log(req.query.steamids)
     request(requestObj).pipe(res);
   })
 }
